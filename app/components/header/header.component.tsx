84633d6fa86016366115b66139b45b14184365d3
Valentin Bourreau <valentin.bourreau@gmail.com>
2023-02-23 18:42:28 +0100

    init: 🚀 launch project

---
+++ app/components/header/header.component.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import valentinbourreau from '../../../public/valentinbourreau.svg';
+import styles from './header.module.css';
+
+export const Header = () => {
+    return <header className={styles.header}>
+        <div className="container">
+            <div className={styles.logo}>
+                {/* <Image src={valentinbourreau} alt="valentin bourreau logo" width={162} height={14} priority /> */}
+            </div>
+        </div>
+    </header>
+}
